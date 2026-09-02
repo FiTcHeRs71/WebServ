2c7d87072ac7addfd073bd990b7457d3ab7af87b
Frederic Ducrot <fducrot@c2r7s2.42lausanne.ch>
2026-09-02 16:20:05 +0200

    feat(http): html escape && urt encoding(C-07)

--- srcs/http/Autoindex.cpp
+++ srcs/http/Autoindex.cpp
@@ -1,4 +1,7 @@
 #include "../../includes/Autoindex.hpp"
+#include <algorithm>
+#include <cctype>
+#include <cstddef>
 #include <sstream>
 #include <string>
 #include <vector>
@@ -34,9 +37,9 @@ static bool	list_dir(const string &dir_path, vector<TDirEntry> &out)
 
 bool	operator<(const TDirEntry &a, const TDirEntry &b)
 {
-	(void)a;
-	(void)b;
-	return (true);
+	if (a.IsDir != b.IsDir)
+		return (a.IsDir);
+	return (a.Name < b.Name);
 }
 
 string	build_autoindex(const string &dir_path, const string &uri)
@@ -46,20 +49,52 @@ string	build_autoindex(const string &dir_path, const string &uri)
 	if (!list_dir(dir_path, entries))
 		return ("");
 	(void)uri;
-	ostringstream	oss;
-	oss << entries.size();
+	sort(entries.begin(), entries.end());
+	ostringstream oss;
+	for(size_t i = 0; i < entries.size(); i++)
+		oss << html_escape(entries[i].Name) << (entries[i].IsDir ? "/" : "") << "\n";
 	return (oss.str());
 }
 
 string	html_escape(const string &s)
 {
-	(void)s;
-	return ("");
+	string	out;
+
+	for (size_t i = 0; i < s.size(); i++)
+	{
+		char c = s[i];
+
+		switch (c)
+		{
+			case '&': out += "&amp;"; break;
+			case '<': out += "&lt;"; break;
+			case '>': out += "&gt;"; break;
+			case '"': out += "&quot;"; break;
+			case '\'': out += "&#39;"; break;
+			default : out += c;
+		}
+	}
+	return (out);
 }
 
 string	url_encode(const string &s)
 {
-	(void)s;
-	return ("");
+	string	hex = "0123456789ABCDEF";
+	string	out;
+
+	for (size_t i = 0; i < s.size(); i++)
+	{
+		char			c = s[i];
+		unsigned char	uc = (unsigned char) c;
+		if (isalnum(uc) || c == '-' || c == '_' || c == '.' || c == '~')
+			out += c;
+		else
+		{
+			out += '%';
+			out += hex[uc >> 4];
+			out += hex[uc & 0x0F];
+		}
+	}
+	return (out);
 }
 
