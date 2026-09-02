83e525e8063a5d20950836418685ac1ffaafc1f3
Frederic Ducrot <fducrot@c2r7s2.42lausanne.ch>
2026-09-02 11:39:43 +0200

    feat(http): skeletton autoindex header and stubs (C-07)

---
+++ srcs/http/Autoindex.cpp
@@ -0,0 +1,27 @@
+#include "../../includes/Autoindex.hpp"
+
+bool	operator<(const TDirEntry &a, const TDirEntry &b)
+{
+	(void)a;
+	(void)b;
+	return (true);
+}
+
+string	build_autoindex(const string &dir_path, const string &uri)
+{
+	(void)dir_path;
+	(void)uri;
+	return ("TEST");
+}
+
+string	html_escape(const string &s)
+{
+	(void)s;
+	return ("");
+}
+
+string	url_encode(const string &s)
+{
+	(void)s;
+	return ("");
+}
